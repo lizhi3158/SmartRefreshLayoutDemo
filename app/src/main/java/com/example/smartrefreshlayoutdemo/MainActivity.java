d8172815f9cb2da229e5509624f18c8eaaae37ca
lizhi3158 <315854887@qq.com>
2017-07-16 19:53:00 +0800

    First commit

---
+++ app/src/main/java/com/example/smartrefreshlayoutdemo/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.smartrefreshlayoutdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.scwang.smartrefresh.header.MaterialHeader;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.constant.SpinnerStyle;
+import com.scwang.smartrefresh.layout.footer.BallPulseFooter;
+import com.scwang.smartrefresh.layout.listener.OnLoadmoreListener;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final RefreshLayout refreshLayout = (RefreshLayout) findViewById(R.id.refreshLayout);
+        //设置 Header 为 Material风格
+        refreshLayout.setRefreshHeader(new MaterialHeader(this).setShowBezierWave(true));
+        //设置 Footer 为 球脉冲
+        refreshLayout.setRefreshFooter(new BallPulseFooter(this).setSpinnerStyle(SpinnerStyle.Scale));
+        refreshLayout.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh(RefreshLayout refreshlayout) {
+                refreshlayout.finishRefresh(2000);
+            }
+        });
+        refreshLayout.setOnLoadmoreListener(new OnLoadmoreListener() {
+            @Override
+            public void onLoadmore(RefreshLayout refreshlayout) {
+                refreshlayout.finishLoadmore(2000);
+            }
+        });
+    }
+}
